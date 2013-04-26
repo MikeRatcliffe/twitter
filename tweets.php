e796e8c46d3366790d87b1f1dae08492b634b978
MikeRatcliffe <mratcliffe@mozilla.com>
2013-04-26 21:41:38 +0100

    Configure output

--- tweets.php
+++ tweets.php
@@ -1,13 +1,18 @@
-<!-- This file can be copied and modified in a theme directory -->
+<?php
 
-<div id="twitterbox">
-	<ul>
-		<?php foreach ($tweets as $tweet) : ?>
-		<li>
-			<img src="<?php echo htmlspecialchars( $tweet->image_url ); ?>" alt="<?php echo urlencode( Options::get( 'twitter__username' )); ?>">
-			<?php echo $tweet->text . ' @ ' . $tweet->time; ?>
-		</li>
-		<?php endforeach; ?>
-	</ul>
-<p><small>via <a href="http://twitter.com/<?php echo urlencode( Options::get( 'twitter__username' )); ?>">Twitter</a></small></p>
- </div>
+$count = count($tweets);
+
+for ($i = 0; $i < $count; $i++) {
+  if ($i == 0 && $tweets[$i]->text == 'Unable to contact Twitter.') {
+    echo '<div id="tweet0" data-visible="true" class="tweet-content">' .
+         '  Firefox is fast, light and awesome there days ... why not <a href="http://www.mozilla.org/firefox/new/">give it a try?</a>' .
+         '</div>';
+    break;
+  }
+  echo '<div id="tweet' . $i . '" class="tweet-content"';
+  if ($i == 0) {
+    echo ' data-visible="true"';
+  }
+  echo '>' . $tweets[$i]->text . ' - ' . $tweets[$i]->time . '</div>';
+}
+?>
